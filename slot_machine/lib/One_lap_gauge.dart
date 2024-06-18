66fcf2bd9e51bd1405fba7e6096df37a2eb20ebc
nebula <star0324k@gmail.com>
2024-06-18 15:24:50 +0900

    PUSHボタンのアップデート
    スペースキーでボタンを押せるようにしました。

--- slot_machine/lib/One_lap_gauge.dart
+++ slot_machine/lib/One_lap_gauge.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class OneLapGauge extends StatefulWidget {
   const OneLapGauge({super.key});
@@ -10,6 +11,7 @@ class OneLapGauge extends StatefulWidget {
 class _OneLapGaugeState extends State<OneLapGauge>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+  final FocusNode _focusNode = FocusNode(); // FocusNodeを定義
 
   @override
   void initState() {
@@ -23,38 +25,54 @@ class _OneLapGaugeState extends State<OneLapGauge>
   @override
   void dispose() {
     _controller.dispose();
+    _focusNode.dispose();
     super.dispose();
+  }
+
+  void _handleKeyEvent(RawKeyEvent event) {
+    if (event.logicalKey == LogicalKeyboardKey.space &&
+        event.runtimeType == RawKeyDownEvent) {
+      // スペースキーが押された時の処理
+      print('PUSHボタンが押されました (スペースキー)');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // Columnでゲージとボタンを縦に並べる
-      mainAxisAlignment: MainAxisAlignment.center, // 縦方向中央揃え
-      children: [
-        AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return SizedBox(
-              height: 20,
-              child: LinearProgressIndicator(
-                value: _controller.value,
-                backgroundColor: Colors.grey,
-                valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
-              ),
-            );
-          },
-        ),
-        const SizedBox(height: 20), // ゲージとボタンの間隔
-        ElevatedButton(
-          // ElevatedButtonウィジェットでボタンを作成
-          onPressed: () {
-            // ボタンが押された時の処理をここに記述
-            // 例: print('PUSHボタンが押されました');
-          },
-          child: const Text('PUSH'), // ボタンのテキスト
-        ),
-      ],
+    return RawKeyboardListener(
+      focusNode: _focusNode,
+      onKey: _handleKeyEvent,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return SizedBox(
+                height: 20,
+                child: LinearProgressIndicator(
+                  value: _controller.value,
+                  backgroundColor: Colors.grey,
+                  valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
+                ),
+              );
+            },
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            autofocus: true, // ボタンに自動的にフォーカスを当てる
+            onFocusChange: (hasFocus) {
+              if (!hasFocus) {
+                _focusNode.requestFocus(); // フォーカスが外れたら再取得
+              }
+            },
+            onPressed: () {
+              print('PUSHボタンが押されました');
+            },
+            child: const Text('PUSH'),
+          ),
+        ],
+      ),
     );
   }
 }
